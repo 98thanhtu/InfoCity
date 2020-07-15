d5546abf9ca0f024ac5439cfd54643863a500f24
98thanhtu <47029265+98thanhtu@users.noreply.github.com>
2020-07-15 20:34:49 +0700

    Add files via upload

---
+++ Services/LocalMailService.cs
@@ -0,0 +1,24 @@
+﻿using Cityinfo;
+using System.Diagnostics;
+using Infocity.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infocity.Services
+{
+    public class LocalMailService : IMailService
+    {
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
+
+        public void Send(string subject, string message)
+        {
+            //Send mail - output to debug window
+            Debug.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with LocalMailService");
+            Debug.WriteLine($"Subject : {subject}");
+            Debug.WriteLine($"Massage : {message}");
+        }
+    }
+}
